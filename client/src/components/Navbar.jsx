b6ca8b5cc7f179b12fea320c99e2d907e1ec1470
Eduardo Orbe <eduardoorbeb@gmail.com>
2024-09-15 11:44:35 -0700

    responsive fixes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
 
 function Navbar() {
+  useEffect(() => {
+    let menu = document.querySelector("#menu-icon");
+    let navlist = document.querySelector(".nav-res");
+    menu.onclick = () => {
+      menu.classList.toggle("bx-x");
+      navlist.classList.toggle("active");
+    };
+  }, []);
+
   const { isAuthenticated, logout, user } = useAuth();
   return (
     <nav className="bg-zinc-700 my-3 flex justify-between py-5 px-10 rounded-lg">
       <Link to={isAuthenticated ? "/tasks" : "/"}>
         <h1 className="text-2xl font-bold">Tasks Manager</h1>
       </Link>
-      <ul className="flex gap-x-2">
+      <div className="bx bx-menu" id="menu-icon"></div>
+      <ul className="flex gap-x-2 nav-res">
         {isAuthenticated ? (
           <>
-            <li>Welcome <h3 className="font-bold">{user.username}</h3></li>
+            <li>
+              <h3 className="font-bold username1">{user.username}</h3>
+            </li>
             <li>
               <Link
                 className="bg-indigo-500 px-4 py-1 rounded-md"
--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -8,9 +8,9 @@ function TaskCard({ task }) {
   const { deleteTask } = useTasks();
   return (
     <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
-      <header className="flex justify-between">
-        <h1 className="text-2xl font-bold">{task.title}</h1>
-        <div className="flex gap-x-2 items-center">
+      <header className="flex justify-between card-res">
+        <h1 className="text-2xl font-bold word-res">{task.title}</h1>
+        <div className="flex gap-x-2 items-center btn-task-res">
           <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md"
             onClick={() => {
               deleteTask(task.id);
